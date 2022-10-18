f086c418f5f98bce808afa98937821757f08730d
simsaeyeon <syeon603@naver.com>
2022-10-18 16:25:21 +0900

    02

--- main.c
+++ main.c
@@ -5,15 +5,12 @@
 
 int main(void) 
 {
-	int flag=1;
-	while(flag !=0)
+	int i;
+	for (i=0;i<5;i++)
 	{
-		int y;
-		y=3;
-		flag=0;
+		int temp=1;
+		printf("temp=%d\n",temp);
+		temp++;
 	}
-	
-	int y=4;
-	
 	return 0;
 }
